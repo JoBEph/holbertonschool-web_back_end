7267087e069a95f67463d844a49c01b382c2c2ab
JoBEph <jordan.baroux31@gmail.com>
2025-03-24 14:19:25 +0100

    try to fix

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -2,12 +2,12 @@ export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
   }
-  
+
   export const getLast = () => ' is okay';
-  
+
   export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
-  
+
     return combination;
   }
