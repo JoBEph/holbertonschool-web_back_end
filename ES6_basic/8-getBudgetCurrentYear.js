201ddbff646d82db584ae5a0e874cc763f9ff761
JoBEph <jordan.baroux31@gmail.com>
2025-03-24 12:16:32 +0100

    Test correct 8 and push 9

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,15 +1,10 @@
-function getCurrentYear() {
-    const date = new Date();
-    return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const year = getCurrentYear();
-    const budget = {};
-  
-    budget[`income-${year}`] = income;
-    budget[`gdp-${year}`] = gdp;
-    budget[`capita-${year}`] = capita;
-  
-    return budget;
-  }
+const getCurrentYear = () => new Date().getFullYear();
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const year = getCurrentYear();
+  return {
+    [`income-${year}`]: income,
+    [`gdp-${year}`]: gdp,
+    [`capita-${year}`]: capita,
+  };
+}
