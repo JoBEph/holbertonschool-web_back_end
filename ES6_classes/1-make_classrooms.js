22e7c2702d56b1ad3fddb4aa0c3ba444d261d1f1
JoBEph <jordan.baroux31@gmail.com>
2025-03-25 14:09:14 +0100

    correct indent

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -3,7 +3,7 @@ import ClassRoom from './0-classroom';
 export default function initializeRooms() {
   return [
     new ClassRoom(19),
-    new ClassRoom(15),
-    new ClassRoom(10),
+    new ClassRoom(20),
+    new ClassRoom(34),
   ];
 }
