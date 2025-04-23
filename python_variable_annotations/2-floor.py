2bdd2837cd38271d318c13c39b754a04a2c51e6d
JoBEph <jordan.baroux31@gmail.com>
2025-04-23 10:44:30 +0200

    function return the floor of the float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import math
+
+"""  function returns the floor of the float """
+
+
+def floor(n: float) -> int:
+    """ Return the floor of a float """
+    return int(math.floor(n))
