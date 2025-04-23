642d48c304dd32f4f153e9445901432367132edb
JoBEph <jordan.baroux31@gmail.com>
2025-04-23 11:21:56 +0200

    function take string, int or float and return tulpe

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,8 @@
 """ function to_kv that takes a string k and an int OR float v
  as arguments and returns a tuple."""
 from typing import Tuple, Union
+
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return a tuple  k and the  square of v."""
     return (k, v * v)
