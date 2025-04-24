434ab8b84410473554249a952146a259b854f0fb
JoBEph <jordan.baroux31@gmail.com>
2025-04-24 10:20:18 +0200

    concurrent coroutines

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """random delay between 0 and max_delay"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """wait for n random delays and return a list of the delays"""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
