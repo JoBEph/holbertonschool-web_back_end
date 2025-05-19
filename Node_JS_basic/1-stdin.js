41461c556cdcc61db3b0b76d35991b80fddeb4c1
JoBEph <jordan.baroux31@gmail.com>
2025-05-19 10:34:17 +0200

    stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+readline.question('', (name) => {
+  console.log(`Your name is: ${name}`);
+  readline.close();
+
+  console.log('This important software is now closing');
+});
