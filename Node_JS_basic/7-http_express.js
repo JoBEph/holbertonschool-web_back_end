c26e4a06173770d7fd87eabf772cbf243e1f1925
JoBEph <jordan.baroux31@gmail.com>
2025-05-20 16:04:25 +0200

    fix undefined

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,42 +1,39 @@
-#!/usr/bin/node
-
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
 const port = 1245;
-const databasePath = process.argv[2];
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-  res.set('Content-Type', 'text/plain');
-
-  if (!databasePath) {
-    res.send('This is the list of our students\nCannot load the database');
+  const database = process.argv[2];
+  if (!database) {
+    res.send('Cannot load the database');
     return;
   }
 
-  let capturedOutput = '';
-  const originalConsoleLog = console.log;
-  console.log = (message) => {
-    capturedOutput += message + '\n';
-  };
-
   try {
-    await countStudents(databasePath);
-    res.send(`This is the list of our students\n${capturedOutput.trim()}`);
-  } catch (error) {
-    res.send('This is the list of our students\nCannot load the database');
-  } finally {
+    const originalConsoleLog = console.log;
+    let response = '';
+
+    console.log = (message) => {
+      response += `${message}\n`;
+    };
+
+    await countStudents(database);
     console.log = originalConsoleLog;
+
+    res.send(response.trim());
+  } catch (error) {
+    res.send(error.message);
   }
 });
 
 app.listen(port, () => {
-  // console.log(`Server listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
