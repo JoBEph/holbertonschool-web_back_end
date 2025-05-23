ad1da0dc97747701a8970ea3d0f0e6cb6a3a3c82
JoBEph <jordan.baroux31@gmail.com>
2025-05-23 09:06:54 +0200

    fix 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,35 +1,30 @@
-const express = require('express');
-const countStudents = require('./3-read_file_async');
+// This module to create a small HTTP server using Express module
+
+const express = require("express");
+const countStudents = require("./3-read_file_async");
 
 const app = express();
 const port = 1245;
+const databaseFile = process.argv[2];
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+app.get("/", (req, res) => {
+  res.set("Content-Type", "text/plain");
+  res.send("Hello Holberton School!");
 });
 
-app.get('/students', async (req, res) => {
-  const database = process.argv[2];
-  if (!database) {
-    res.send('Cannot load the database');
-    return;
-  }
+app.get("/students", async (req, res) => {
+  res.set("Content-Type", "text/plain");
+
+  let responseText = "This is the list of our students";
 
   try {
-    const originalConsoleLog = console.log;
-    let response = '';
-
-    console.log = (message) => {
-      response += `${message}\n`;
-    };
-
-    await countStudents(database);
-    console.log = originalConsoleLog;
-
-    res.send(response.trim());
+    const studentData = await countStudents(databaseFile);
+    responseText += `\n${studentData}`;
   } catch (error) {
-    res.send(error.message);
+    responseText += `\n${error.message}`;
   }
+
+  res.send(responseText.trimEnd());
 });
 
 app.listen(port, () => {
